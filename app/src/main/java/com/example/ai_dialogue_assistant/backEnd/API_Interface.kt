45342aa238ef59a452874895c66926e936b0f551
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2024-08-02 01:10:01 -0400

    removed unmade call from backend

--- app/src/main/java/com/example/ai_dialogue_assistant/backEnd/API_Interface.kt
+++ app/src/main/java/com/example/ai_dialogue_assistant/backEnd/API_Interface.kt
@@ -1,27 +1,15 @@
 package com.example.ai_dialogue_assistant.backEnd
 
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface API_Interface {
-    @Multipart
-    @POST("generate")
-    fun generateContentWithImage(
-        @Part("text") text: RequestBody,
-        @Part image: MultipartBody.Part?
-    ): Call<ResponseBody>
-
     @POST("/conversations")
     fun createConversation(@Body conversation: Conversation): Call<Conversation>
 
