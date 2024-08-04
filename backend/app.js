19b4feaa71cf5c3f2b43f26b6fcad3c9c3ffd076
kevin-rodri <kr399880@gmail.com>
2024-08-03 21:34:10 -0400

    change

--- backend/app.js
+++ backend/app.js
@@ -42,8 +42,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // required routers 
 app.use('/conversations', conversationRouter);
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
